3feda04b1a7c4f167b05520ada60fd681d3ba4d7
Aluno <luis.silva8494@gmail.com>
2025-04-15 14:51:35 -0300

    VAMO PO

--- veterinario/resources/views/home.blade.php
+++ veterinario/resources/views/home.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    @vite(['resources/css/appHome.css', 'resources/js/appHome.js'])
-</head>
-<body>
-    
-</body>
-</html>
+<div>
+    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
+</div>
