9a7e5680fbdfa41fd760cf4f49b3ca2c92d34d3b
Aluno <you@example.com>
2025-05-20 10:12:09 -0300

    Adicionada estrutura básica do CRUD

---
+++ veterinario/resources/views/clientes.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clientes</title>
+</head>
+<body>
+    @foreach($clientes as $cliente)
+        {{ $cliente->nome }}
+    @endforeach
+</body>
+</html>
--- veterinario/routes/web.php
+++ veterinario/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CadastroController;
 use App\Http\Controllers\Cadastro2Controller;
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\RegisterController;
 
 
@@ -12,4 +13,6 @@ Route::get('/', function () {
 Route::get('/veterinario',[CadastroController::class, 'cadastro'])->name('veterinario.cadastro');
 Route::get('/teste',[cadastro2Controller::class, 'home'])->name('teste.home');
 Route::get('/registros', [RegisterController::class, 'registros'])->name('registro.site');
+
+Route::resource('clientes', ClienteController::class);
 
